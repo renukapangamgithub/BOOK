4d7c997a5d736eb39ab237af2cef040360f44359
renukapangamgithub <159953063+renukapangamgithub@users.noreply.github.com>
2024-12-26 19:19:16 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ import contactRoute from "./route/contact.route.js";
 const app=express();
 app.use(cors(
     {
-        origin:["https://deploy-mern-1whq.vercel.app"],
+        origin:[""],
         methods:["POST","GET"],
         crendentials:true
     }
