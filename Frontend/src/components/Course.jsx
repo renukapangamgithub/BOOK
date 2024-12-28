178a42a1a74b534d1cbf158e6169b7166d805ea7
Renuka Pangam <159953063+renukapangamgithub@users.noreply.github.com>
2024-12-28 21:41:26 +0530

    Update Course.jsx

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -9,7 +9,7 @@ function Course() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("https://book-store-ltpo.vercel.app/book");
+        const res = await axios.get("https://book-api-beryl.vercel.app/book");
         console.log(res.data);
         setBook(res.data);
       } catch (error) {
