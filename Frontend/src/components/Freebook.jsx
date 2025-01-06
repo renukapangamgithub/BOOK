48b5148449593ea2a97e85ca5a6fd0b12a18b103
Renuka Pangam <159953063+renukapangamgithub@users.noreply.github.com>
2025-01-06 21:54:19 +0530

    Update Freebook.jsx

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -14,7 +14,7 @@ function Freebook() {
     const getBook = async () => {
       try {
         
-        const res = await axios.get("https://book-backend-wnkq.onrender.com");
+        const res = await axios.get("https://book-backend-wnkq.onrender.com/book");
        
         const data = res.data.filter((data) => data.category === "free");
         console.log(data);
